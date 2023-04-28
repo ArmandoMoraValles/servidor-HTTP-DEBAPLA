06f45d7f302fef90336d3f08451adada696d7f38
armandomoravalles <armandomoravalles@gmail.com>
2023-04-27 22:18:32 -0600

    3) Servidor síncrono que lea y envíe un archivo

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+  try {
+    const fileData = fs.readFileSync('file.txt');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(fileData);
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.end('Error interno del servidor');
+  }
 });
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
