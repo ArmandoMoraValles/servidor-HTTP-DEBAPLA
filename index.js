3043156720293e60494d1d55abaee0d91b81fefa
armandomoravalles <armandomoravalles@gmail.com>
2023-04-27 22:39:15 -0600

    4) Servidor asíncrono que lea y envíe un archivo.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const http = require('http');
 const fs = require('fs');
+
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  try {
-    const fileData = fs.readFileSync('file.txt');
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(fileData);
-  } catch (error) {
-    console.error(error);
-    res.statusCode = 500;
-    res.end('Error interno del servidor');
-  }
+  // Es asincrono gracias al callback, 
+  fs.readFile('file.txt', (err, data) => {
+    // Basicamente una funcion que se ejecuta despues de que otra termina, por eso es asincrono
+    if (err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end('Error interno del servidor');
+    } else {
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, () => {
